05f1c41e1466283a537427f42cea28901c36a66e
Menzi Khuzwayo <asakhuzwayo@gmail.com>
2019-11-15 15:19:43 +0200

    Added a static class and member to check if the word is equal is the same when reversed.

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using palindrome.WordChecker;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace palindrome
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter word to check : ");
+            var wordToCheck=Console.ReadLine();
+
         }
     }
 }
---
+++ palindrome/WordChecker/wordChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome.WordChecker
+{
+    public static class wordChecker
+    {
+        public static bool checkWord(string word)
+        {
+            return word.SequenceEqual(word.Reverse());
+        }
+    }
+}
